b1f6e77866f024dd9ea5d2136cddaf36baf58c30
01one <84787013+01one@users.noreply.github.com>
2021-08-19 03:19:48 +0600

    Create mathematical_relation_among_power.py

---
+++ My_Calculation/mathematical_relation_among_power.py
@@ -0,0 +1,25 @@
+#  Copyright 2021 Mahid   e-mahid@outlook.com
+#  I found a replationship among power when I was a college student.
+#  I do not know the importance of this. But thers an interesting pattern.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY
+
+
+def power(p=2):
+	x=[i**p for i in range(1,p+2)]
+	print(x)
+	n=[]
+	for i in range(p):
+		for j in range(len(x)-1):
+			a=abs(x[j]-x[j+1])
+			n.append(a)
+		x=n
+		print(x)
+		n=[]
+power(2)
+power(3)
+power(4)
+power(5)
+power(6)
+power(7)
