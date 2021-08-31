e96ae48b82189a0efe500e5a426cf7a2e6a52c18
01one <84787013+01one@users.noreply.github.com>
2021-08-31 06:39:52 +0600

    Create goldbach_conjecture.py

---
+++ conjecture/goldbach_conjecture.py
@@ -0,0 +1,30 @@
+#  Copyright 2021 Mahid e-mahid@outlook.com
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY
+
+
+"""Goldbach’s conjecture
+Any even number can be written as a sum of two primes."""
+
+x=eval(input("Enter an even number: "))
+prime=[]
+
+for i in range(2,x+1):
+	for j in range(2,i):
+		if i%j==0:
+			break
+	else:
+		prime.append(i)
+		
+y=len(prime)
+
+l=[]
+
+for i in range(y):
+	for j in range(i):
+		a=prime[i]
+		b=prime[j]
+		k=a+b
+		if k==x:
+			l.append([a,b])
+print(l)
